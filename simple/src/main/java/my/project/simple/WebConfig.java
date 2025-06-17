caac5b954d1917cb96b143c580782044be061396
donggyu <leedong1848@gmail.com>
2025-06-17 11:48:23 +0900

    FIX: Work well CORS Setting

--- simple/src/main/java/my/project/simple/WebConfig.java
+++ simple/src/main/java/my/project/simple/WebConfig.java
@@ -1,15 +1,23 @@
 package my.project.simple;
 
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
+
+	@Value("${cors.allowed}")
+	private String allowedOrigins;
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")                 // 모든 경로에 대해
-				.allowedOrigins("http://localhost:5173")  // 허용할 도메인들
+				.allowedOrigins(allowedOrigins)  // 허용할 도메인들
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")      // 허용할 HTTP 메서드
 				.allowedHeaders("*")                // 허용할 헤더
 				.allowCredentials(true)             // 인증정보 허용 (쿠키 등)
